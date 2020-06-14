df7b7e59a90ae81f1d1df2d58e601dc60f42d11f
Hossein Mohammadi <hussein.muhammadi@hotmail.com>
2020-06-14 15:22:59 +0430

    Spring boot multi module project

---
+++ web-api/src/main/java/ir/mebank/isc/controller/HomeController.java
@@ -0,0 +1,38 @@
+package ir.mebank.isc.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public ResponseEntity<Person> index() {
+        return ResponseEntity.ok(new Person());
+    }
+}
+
+class Person {
+    String name = "Ali";
+    String family = "Mohammadi";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+}
